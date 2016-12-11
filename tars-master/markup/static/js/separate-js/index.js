7ab818a1bf39a820ab79c4eabead0a928eb49929
prince-conte <prince-conte@outlook.com>
2016-12-11 19:27:49 +0300

    +js-for-menu

--- tars-master/markup/static/js/separate-js/index.js
+++ tars-master/markup/static/js/separate-js/index.js
@@ -16,12 +16,15 @@ function openMenu() {
     if (openMenuClick == 0) {
     
         $("#mobile_menu").fadeIn(500);
+        $("html").css('overflow' , 'hidden');
         openMenuClick = 1;
+        
 }
 
     else {
         
         $("#mobile_menu").fadeOut(500);
+        $("html").css('overflow' , 'auto');
         openMenuClick = 0;
         
         
