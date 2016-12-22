c425855b5d709f1becc6ca20a37eab618d64fa63
francesco <prince-conte@outlook.com>
2016-12-22 17:51:37 +0300

    ver:1.5.0.1(jobs+)

---
+++ tars-master/builds/build_ver-Thu_Dec_22_2016_10_59_19/static/js/separate-js/index.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    
+SocialShareKit.init();
+
+centerSlider()  
+
+$("#footer_smb").click(function() {
+    
+    $("#footer_smb_inp").trigger('click');
+});
+    
+    
+
+   
+$("#header_region_popup").click(function() {
+    openRegion();  
+});
+    
+    
+$("#header_region_close").click(function() {   
+    openRegion();
+});
+    
+      
+   
+$("#header_login_popup").click(function() {
+    openLogin();  
+});
+    
+    
+$("#header_login_close").click(function() {   
+    openLogin();
+});
+    
+
+    
+    
+   
+$("#openmenu_button").click(function() {
+
+openMenu()
+
+});
+    
+    
+$("#closemenu_button").click(function() {   
+
+openMenu()
+});
+    
+     
+      
+
+   
+
+
+    
+});
+
