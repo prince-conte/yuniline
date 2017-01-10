546c2999a88f4c9a238587132ceb6080578e8c75
francesco <prince-conte@outlook.com>
2017-01-10 18:00:16 +0300

    ver:1.6.0.1(dep)

--- tars-master/markup/components/category/data/data.js
+++ tars-master/markup/components/category/data/data.js
@@ -3,19 +3,29 @@ category: {
     news: {
         categoryNews: true,
         categoryIsdatelstvo: false,
-        categoryJobs: false
+        categoryJobs: false,
+        categoryDepartments: false
     },
      
     isdatelstvo: {
         categoryIsdatelstvo: true,
         categoryNews: false,
-        categoryJobs: false
+        categoryJobs: false,
+        categoryDepartments: false
     },
          
     jobs: {
         categoryIsdatelstvo: false,
         categoryNews: false,
-        categoryJobs: true
+        categoryJobs: true,
+        categoryDepartments: false
+    },
+             
+    departments: {
+        categoryDepartments: false,
+        categoryNews: false,
+        categoryJobs: false,
+        categoryDepartments: true
     },
     
 }
---
+++ tars-master/markup/components/formjobs/data/data.js
@@ -0,0 +1,11 @@
+jobsarticle: {
+ 
+    categoryTrue: {
+        useCategory: true,
+    },
+     
+    categoryFalse: {
+        useCategory: false,
+    },
+    
+}
