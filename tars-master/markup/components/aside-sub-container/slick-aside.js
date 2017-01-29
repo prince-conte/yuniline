5be20d59bb5edf48d4257811d67754788e3672a1
prince-conte <prince-conte@outlook.com>
2017-01-29 17:21:54 +0300

    правочки

--- tars-master/markup/components/aside-sub-container/slick-aside.js
+++ tars-master/markup/components/aside-sub-container/slick-aside.js
@@ -16,9 +16,7 @@ $(window).scroll(function() {
 	var mb = 800; /* высота подвала с запасом */
 	var st = $(window).scrollTop();
 	var sb = $(".sticky-block");
-	var sbi = $(".sticky-block");
 	var sb_ot = sb.offset().top;
-	var sbi_ot = sbi.offset().top;
 	var sb_h = sb.height();
  
 	if(sb_h + $(document).scrollTop() + sb_m + mb < $(document).height()) {
