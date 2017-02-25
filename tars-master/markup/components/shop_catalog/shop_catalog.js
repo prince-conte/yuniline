c76d790c0d5ffc7af1e99fcb0845c519fd3b836e
prince-conte <prince-conte@outlook.com>
2017-02-25 23:39:42 +0300

    + shopgrid 2.15.0.0.5

---
+++ tars-master/markup/components/shop_catalog/shop_catalog.js
@@ -0,0 +1,11 @@
+function shopSlider() {  
+ 
+$('.promo__shop-slider').slick({
+  dots: false,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+    
+};    
+     
--- tars-master/markup/static/js/separate-js/index.js
+++ tars-master/markup/static/js/separate-js/index.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
  
 conteReg();   
     
+shopSlider();
     
 centerSlider(); 
     
