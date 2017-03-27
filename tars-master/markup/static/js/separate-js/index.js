1140d0e120f4e7170c2f116e8ceec0cb072dc8c2
prince-conte <prince-conte@outlook.com>
2017-03-27 22:20:26 +0300

    yap

--- tars-master/markup/static/js/separate-js/index.js
+++ tars-master/markup/static/js/separate-js/index.js
@@ -30,6 +30,24 @@ bascetIn ();
 openAddres();
 	
 selectBasket();
+    
+
+$('.minus').click(function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+    });
+    
+$('.plus').click(function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+});
+
 
 $("#footer_smb").click(function() {
     
