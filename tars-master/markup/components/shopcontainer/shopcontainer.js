a0ae5268750f2018e21b7504b4247ee96526f66d
prince-conte <prince-conte@outlook.com>
2017-04-01 18:43:17 +0300

    yappp

--- tars-master/markup/components/shopcontainer/shopcontainer.js
+++ tars-master/markup/components/shopcontainer/shopcontainer.js
@@ -1,5 +1,4 @@
-
- $('.tovar-slider').slick({
+$('.tovar-slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
@@ -14,54 +13,6 @@ $('.slider-nav').slick({
   centerMode: false,
   focusOnSelect: true
 });
-
-//
-//
-//function total () {
-//    
-//    var total = 0; 
-//    
-//    monthMoney_1 = parseInt($('#js-month1 input').val())
-//    monthMoney_2 = parseInt($('#js-month2 input').val())
-//    monthMoney_3 = parseInt($('#js-month3 input').val())
-//    monthMoney_4 = parseInt($('#js-month4 input').val())
-//    monthMoney_5 = parseInt($('#js-month5 input').val())
-//    monthMoney_6 = parseInt($('#js-month6 input').val())
-//    monthMoney_7 = parseInt($('#js-month7 input').val())
-//    monthMoney_8 = parseInt($('#js-month8 input').val())
-//    monthMoney_9 = parseInt($('#js-month9 input').val())
-//    monthMoney_10 = parseInt($('#js-month10 input').val())
-//    monthMoney_11 = parseInt($('#js-month11 input').val())
-//    monthMoney_12 = parseInt($('#js-month12 input').val())
-//    
-//    
-//    
-//    total = monthMoney_1 + monthMoney_2 + monthMoney_3 + monthMoney_4 + monthMoney_5 + monthMoney_6 + monthMoney_7 + monthMoney_8 + monthMoney_9 + monthMoney_10 + monthMoney_11 + monthMoney_12
-//    alert(total);
-//    
-//} 
-//
-
-
-
-function total(){
-   var total=0;
-   var itogTotalPrice;
-   var price = $('#price').html();
-    
-   $("#js-subscribe label input").each(function(){
-      total=total+parseInt($(this).val())
-   });
-    
-   itogTotalPrice = total * price; 
-    
-    
-    $('#itog-price-element').text(itogTotalPrice.toFixed(2));
-
-    
-   return total;
-}
-
 
 function bascetIn () {
     
